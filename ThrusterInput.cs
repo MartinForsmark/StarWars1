1753f5dadd2b1a3c6c06a5f5a6e5f652e7ebfae2
MartinForsmark <70843057+MartinForsmark@users.noreply.github.com>
2020-09-17 08:55:33 +0200

    Add files via upload

---
+++ ThrusterInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ThrusterInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetComponent<Thruster>().enabled = Input.GetKey(KeyCode.Space);
+    }
+}
